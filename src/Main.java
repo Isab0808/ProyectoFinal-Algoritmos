d40040129d3c09b639016f6c3206404079b18599
Luisa Marinez <luisa.marinez1@correo.icesi.edu.co>
2021-06-01 21:01:18 -0500

    subiendo imagenes

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,38 @@ public class Main extends PApplet {
 
 	
 	public void settings() {
-		size(1000, 600);
+		size(1200, 700);
 	}
 	
-		
+	PImage[] pantalla;
+	PImage[] niveles;
+	
+	//muros
+	PImage muro1;
+	PImage muro2;
+	PImage muro3;
+	PImage muro4;
+
 		
 	public void setup() {
 		
+	muro1 = loadImage("muro1.jpg");	
+	muro2 = loadImage("muro2.jpg");	
+	muro3 = loadImage("muro3.jpg");	
+	muro4 = loadImage("muro4.jpg");	
+			
+	pantalla = new PImage[5];
+	pantalla[0] = loadImage("Pantallainicio.jpg");	
+	pantalla[1] = loadImage("PantallaLogin.jpg");	
+	pantalla[2] = loadImage("PantallaRegistro.jpg");	
+	pantalla[3] = loadImage("Instrucciones1.jpg");	
+	pantalla[4] = loadImage("Instrucciones2.jpg");	
+
+	niveles = new PImage[4];
+	niveles[0] = loadImage("Nivel1.jpg");	
+	niveles[1] = loadImage("Nivel2.jpg");	
+	niveles[2] = loadImage("Nivel3.jpg");	
+	niveles[3] = loadImage("Nivel4.jpg");	
 
 	}
 	
@@ -30,7 +55,6 @@ public class Main extends PApplet {
 	
 	
 public void keyPressed() {
-		
 	
 	}
 	
